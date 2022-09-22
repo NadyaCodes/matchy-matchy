bdd583dfb82c47cfff22e54e69504f54ef41e9da
NadyaCodes <nadyacorscadden@gmail.com>
2022-09-22 19:17:52 -0400

    flip cards on click

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react"
+
+
 export default function Card() {
+  const [back, setBack] = useState(true)
+
   return(
-    <div className='card'></div>
+  <div>
+    {back === true 
+    ? 
+    <div className='card card-back' onClick={() => setBack(false)}></div> 
+    : 
+    <div className='card card-front' onClick={() => setBack(true)}></div>}
+  </div>
+    
   )
 }
--- src/CardDisplay.jsx
+++ src/CardDisplay.jsx
@@ -1,10 +1,11 @@
 import Card from "./Card"
+
 export default function CardDisplay(props) {
   const {num} = props
   let allCards = []
 
   for (let i = 0; i < num; i++) {
-    allCards.push(<Card key={i} />)
+    allCards.push(<Card key={i}/>)
   }
 
   let displayClass = `cardDisplay-${num}`
