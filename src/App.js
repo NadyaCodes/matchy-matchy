dea5cb1387dc0d611fadd0392f276f870c6610b3
NadyaCodes <nadyacorscadden@gmail.com>
2022-09-22 10:22:31 -0400

    display blank cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import CardDisplay from './CardDisplay';
 
 function App() {
+  const [state, setState] = useState({num: 30})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setState((prev) => ({...prev, num: 12}))}>12</button>
+      <button onClick={() => setState((prev) => ({...prev, num: 30}))}>30</button>
+      <CardDisplay num={state.num} />
     </div>
-  );
+    
+  )
 }
 
 export default App;
---
+++ src/CardDisplay.jsx
@@ -0,0 +1,15 @@
+import Card from "./Card"
+export default function CardDisplay(props) {
+  const {num} = props
+  let allCards = []
+
+  for (let i = 0; i < num; i++) {
+    allCards.push(<Card key={i} />)
+  }
+
+  let displayClass = `cardDisplay-${num}`
+
+  return(
+    <div className={displayClass}>{allCards}</div>
+  )
+}
