d4570cf3af3f2141c71c4c273ffd8894b84f7414
NadyaCodes <nadyacorscadden@gmail.com>
2022-09-22 23:28:20 -0400

    add dead card functionality

--- src/CardDisplay.jsx
+++ src/CardDisplay.jsx
@@ -18,23 +18,37 @@ export default function CardDisplay(props) {
     const newState = [...cardState]
     newState[index].display = 'front'
     setCardState(newState)
-    setFlippedArray((prev) => [...prev, index])
+    setFlippedArray((prev) => [...prev, newState[index]])
+
+    if (flippedArray.length > 0 && cardState[index].img === flippedArray[0].img) {
+      setTimeout(() => {
+      const deadState = [...cardState]
+      deadState[index].display = 'dead'
+      const deadIndex = cardState.findIndex(element => { return element.display === 'front'})
+      deadState[deadIndex].display = 'dead'
+
+        setCardState(deadState)
+        setFlippedArray([])
+      }, 500)
+    }
+
+
+    else if (flippedArray.length > 0) {
+      setTimeout(() => {
+        const revertState = [...cardState]
+        revertState[index].display = 'back'
+        const lastIndex = cardState.findIndex(element => { return element.display === 'front'})
+        revertState[lastIndex].display = 'back'
+        setCardState(revertState)
+        setFlippedArray([])
+      }, 500)
+    }
   }
 
   const cardDisplay = cardState.map((card, index) => {
     return <Card key={index} id={index} display={card.display} img={card.img} flip={() => flip(index)} />
   })
 
-
-useEffect(() => {
-  if (flippedArray.length > 1) {
-    setTimeout(() => {
-      setCardState(cardArray)
-      setFlippedArray([])
-    }, 500)
-  }
-
-}, [flippedArray, cardArray])
 
 
   let displayClass = `cardDisplay-${num}`
