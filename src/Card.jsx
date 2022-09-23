bcf54b26d0a0ed76e0d9a72a8f1a0c07df5df2cf
NadyaCodes <nadyacorscadden@gmail.com>
2022-09-22 21:36:53 -0400

    move flip to card display component and limit flipping to 2 cards at once

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,17 +1,30 @@
 import { useState } from "react"
 
 
-export default function Card() {
-  const [back, setBack] = useState(true)
+export default function Card(props) {
+  // const [back, setBack] = useState(true)
 
+  const {id, back, flip, img} = props
   return(
-  <div>
-    {back === true 
-    ? 
-    <div className='card card-back' onClick={() => setBack(false)}></div> 
-    : 
-    <div className='card card-front' onClick={() => setBack(true)}></div>}
-  </div>
+  // <div>
+  //   {props.back === true 
+  //   ? 
+  //   <div className='card card-back' onClick={() => setBack(false)}></div> 
+  //   : 
+  //   <div className='card card-front' onClick={() => setBack(true)}></div>}
+
+  //   {/* <div className='card'></div> */}
+  // </div>
+
+<div>
+{back === false 
+? 
+<div className='card card-back' onClick={flip}></div> 
+: 
+<div className='card card-front' onClick={flip}></div>}
+
+{/* <div className='card'></div> */}
+</div>
     
   )
 }
--- src/CardDisplay.jsx
+++ src/CardDisplay.jsx
@@ -1,16 +1,44 @@
 import Card from "./Card"
+import { useState, useEffect } from "react"
 
 export default function CardDisplay(props) {
   const {num} = props
-  let allCards = []
+
+  let cardArray = []
 
   for (let i = 0; i < num; i++) {
-    allCards.push(<Card key={i}/>)
+    cardArray.push({back: false, img: 'image'})
   }
+
+  const [cardState, setCardState] = useState(cardArray)
+  const [flippedArray, setFlippedArray] = useState([])
+
+  const flip = (index) => {
+    const newState = [...cardState]
+    newState[index].back = true
+    setCardState(newState)
+    setFlippedArray((prev) => [...prev, index])
+  }
+
+  const cardDisplay = cardState.map((card, index) => {
+    return <Card key={index} id={index} back={card.back} img={card.image} flip={() => flip(index)} />
+  })
+
+
+useEffect(() => {
+  if (flippedArray.length > 1) {
+    setTimeout(() => {
+      setCardState(cardArray)
+      setFlippedArray([])
+    }, 500)
+  }
+
+}, [flippedArray])
+
 
   let displayClass = `cardDisplay-${num}`
 
   return(
-    <div className={displayClass}>{allCards}</div>
+    <div className={displayClass}>{cardDisplay}</div>
   )
 }
